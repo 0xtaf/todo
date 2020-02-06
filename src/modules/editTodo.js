d9c26d7f248455b5568653e552e41ac1e8f9ad53
Tayfun Sur <tayfunsur@gmail.com>
2020-02-06 19:33:33 +0300

    fixed the bugs in local storage

--- src/modules/editTodo.js
+++ src/modules/editTodo.js
@@ -1,52 +1,25 @@
 import {myProjects, index} from './pageLoadLogic';
 import renderToDosFunc from './renderToDos';
-function storageAvailable(type) {
-    var storage;
-    try {
-        storage = window[type];
-        var x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            (storage && storage.length !== 0);
-    }
-}
-Storage.prototype.setObj = function(key, obj) {
-    return this.setItem(key, JSON.stringify(obj))
-}
-Storage.prototype.getObj = function(key) {
-    return JSON.parse(this.getItem(key))
-}
+import storageAvailable from './localStorage';
+storageAvailable();
 
-if (storageAvailable('localStorage')) {
-    console.log("storage is available")
-}
-else {
-    console.log("storage is not available")
-}
 function editTodoFunc(){  
+    
     const tdNameCell = [...document.querySelectorAll('.nameCell')]
     let counter = 0;
     tdNameCell.forEach(item => {
-        
+        console.log("indexno")
+        console.log(index.indexNo)
+        console.log("dataset imdex")
+        console.log(this.parentNode.dataset.index)
         if (counter == this.parentNode.dataset.index){
             myProjects[index.indexNo]['todoArray'][this.parentNode.dataset.index] = item.textContent
         }
         
         counter++;
+        if (storageAvailable('localStorage')) {
+            localStorage.setObj("myProjects", myProjects);
+        }
     });
     if (storageAvailable('localStorage')) {
         localStorage.setObj("myProjects", myProjects);
--- src/modules/pageLoadLogic.js
+++ src/modules/pageLoadLogic.js
@@ -2,6 +2,7 @@ import CreateProjects from './createProjects';
 import renderProjectsFunc from './renderProjects';
 import renderToDosFunc from './renderToDos';
 import getProjectIndex from './choseProject';
+import storageAvailable from './localStorage';
 
 let myProjects = [];
 
@@ -13,43 +14,8 @@ let index = {
     indexNo:-1,
     priority: 0
 };
-function storageAvailable(type) {
-    var storage;
-    try {
-        storage = window[type];
-        var x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    }
-    catch(e) {
-        return e instanceof DOMException && (
-            // everything except Firefox
-            e.code === 22 ||
-            // Firefox
-            e.code === 1014 ||
-            // test name field too, because code might not be present
-            // everything except Firefox
-            e.name === 'QuotaExceededError' ||
-            // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-            // acknowledge QuotaExceededError only if there's something already stored
-            (storage && storage.length !== 0);
-    }
-}
-Storage.prototype.setObj = function(key, obj) {
-    return this.setItem(key, JSON.stringify(obj))
-}
-Storage.prototype.getObj = function(key) {
-    return JSON.parse(this.getItem(key))
-}
+storageAvailable();
 
-if (storageAvailable('localStorage')) {
-    console.log("storage is available")
-}
-else {
-    console.log("storage is not available")
-}
 if (storageAvailable('localStorage')) {
     if(localStorage.length != 0){
 		   myProjects = localStorage.getObj("myProjects");
