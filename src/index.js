173aa2c97383ddcc2e39b995d6981182edcc1763
Tayfun Sur <tayfunsur@gmail.com>
2020-02-08 09:26:27 +0300

    editing function is fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import PageLoad from './modules/pageLoad';
-import {myProjects, index} from './modules/pageLoadLogic';
-import CreateProjects from './modules/createProjects';
-import log from './modules/trial';
 import {addProjectButtonFunc, closeForm, submitForm} from './modules/addProjectButton';
 import addTodo from './modules/addTodoButton';
 
--- src/modules/editTodo.js
+++ src/modules/editTodo.js
@@ -3,15 +3,13 @@ import storageAvailable from './localStorage';
 storageAvailable();
 
 function editTodoFunc(){  
+    console.log("it's in edittodo")
     
     const tdNameCell = [...document.querySelectorAll('.nameCell')]
-    let counter = 0;
+    
     tdNameCell.forEach(item => {
-        if (counter == this.parentNode.dataset.index){
-            myProjects[index.indexNo]['todoArray'][this.parentNode.dataset.index] = item.textContent
-        }
+        myProjects[index.indexNo]['todoArray'][this.parentNode.dataset.index] = item.textContent
         
-        counter++;
         if (storageAvailable('localStorage')) {
             localStorage.setObj("myProjects", myProjects);
         }
